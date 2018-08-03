f85de023cdac8583c1d82ac3a50fb3dc57af8a3e
Ravinder Mandal <ravi.mandal@6Point6.co.uk>
2018-08-03 18:31:43 +0100

    adding jenkins file to root

--- seed_jobs/FirstJob.groovy
+++ seed_jobs/FirstJob.groovy
@@ -1,10 +1,5 @@
 pipelineJob("my first seed job"){
-    stages{
-        stage("first"){
-            steps{
-                echo("hello world")
-            }
-        }
+    stage("first"){
+        echo "hello world"
     }
-
 }
